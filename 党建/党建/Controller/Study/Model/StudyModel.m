42025156bbd174c95c9531c78dabeed122650efa
CHF <1737834954@qq.com>
2018-10-25 17:09:03 +0800

    CHF搭建模块：随时学

---
+++ 党建/党建/Controller/Study/Model/StudyModel.m
@@ -0,0 +1,13 @@
+//
+//  StudyModel.m
+//  党建
+//
+//  Created by apple on 2018/10/25.
+//  Copyright © 2018年 wx. All rights reserved.
+//
+
+#import "StudyModel.h"
+
+@implementation StudyModel
+
+@end
---
+++ 党建/党建/Controller/Study/View/StudyTableViewCell.h
@@ -0,0 +1,17 @@
+//
+//  StudyTableViewCell.h
+//  党建
+//
+//  Created by apple on 2018/10/25.
+//  Copyright © 2018年 wx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface StudyTableViewCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
