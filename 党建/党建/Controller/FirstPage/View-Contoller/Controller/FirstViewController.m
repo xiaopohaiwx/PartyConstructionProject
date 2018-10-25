0238e217d70cc79067c04dd1e0bfc42050bcc8cb
xiaopohaiwx <1309232650@qq.com>
2018-10-25 20:34:59 +0800

    wx轮播图框架撘完

--- 党建/党建/Controller/FirstPage/View-Contoller/Controller/FirstViewController.m
+++ 党建/党建/Controller/FirstPage/View-Contoller/Controller/FirstViewController.m
@@ -13,6 +13,17 @@
 @end
 
 @implementation FirstViewController
+//懒加载
+-(AFHTTPSessionManager *)manager
+{
+    if(!_manager)
+    {
+        _manager = [[AFHTTPSessionManager alloc] initWithBaseURL:[NSURL URLWithString:@"http://211.67.177.56:8080"]];
+        _manager.requestSerializer = [AFHTTPRequestSerializer serializer];
+        _manager.responseSerializer = [AFJSONResponseSerializer serializer];
+    }
+    return _manager;
+}
 
 //可以放到控制器的View中，自己抽出去
 - (void)initTabbarWireView
@@ -39,12 +50,52 @@
     UIBarButtonItem *rightItem = [[UIBarButtonItem alloc] initWithCustomView:_loginBtn];
     self.navigationItem.rightBarButtonItem = rightItem;
 }
+//初始化scrollView
+- (void)initScrollView
+{
+    _scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0, NavigationBarHeight, SCREENWIDTH, SCREENHEIGHT - NavigationBarHeight - TabBarHeight)];
+    _scrollView.showsVerticalScrollIndicator = NO;
+    [self.view addSubview:_scrollView];
+}
+//轮播图
+- (void)initBanner
+{
+    BannerView *view1 = [[BannerView alloc] init];
+    [view1.imgView setImageWithURL:[NSURL URLWithString:@"http://oowantxlb.bkt.clouddn.com/upload/rear/749b4f2a7cc56b5c0068545718fd24d4.png"] placeholderImage:[UIImage imageNamed:@"placeholderImage"]];
+    view1.labelTitle.text = @"讲形势指方向——图解读习近平这次对省部级干部说了啥";
+    
+    BannerView *view2 = [[BannerView alloc] init];
+    [view2.imgView setImageWithURL:[NSURL URLWithString:@"http://oowantxlb.bkt.clouddn.com/upload/rear/4bbac18c1f8f6c0521b7b6af2ae8ad6c.png"] placeholderImage:[UIImage imageNamed:@"placeholderImage"]];
+    view2.labelTitle.text = @"习近平：“永远做人民公仆、时代先锋、民族脊梁";
+    
+    BannerView *view3 = [[BannerView alloc] init];
+    [view3.imgView setImageWithURL:[NSURL URLWithString:@"http://oowantxlb.bkt.clouddn.com/upload/rear/66e257fe2b5afdbb6acb8ed1127d06c9.png"] placeholderImage:[UIImage imageNamed:@"placeholderImage"]];
+    view3.labelTitle.text = @"校长谭贞在信息工程学院作学习中央31号文件精神专题报告";
+    
+    BannerView *view4 = [[BannerView alloc] init];
+    [view4.imgView setImageWithURL:[NSURL URLWithString:@"http://oowantxlb.bkt.clouddn.com/upload/rear/46296e429c7cda71b941b99fa3461fd1.png"] placeholderImage:[UIImage imageNamed:@"placeholderImage"]];
+    view4.labelTitle.text = @"水調歌頭—慶祝黨的十九大勝利召開";
+    
+    BannerView *view0 = [[BannerView alloc] init];
+    [view0.imgView setImageWithURL:[NSURL URLWithString:@"http://oowantxlb.bkt.clouddn.com/upload/rear/46296e429c7cda71b941b99fa3461fd1.png"] placeholderImage:[UIImage imageNamed:@"placeholderImage"]];
+    view0.labelTitle.text = @"水調歌頭—慶祝黨的十九大勝利召開";
+    
+    BannerView *view5 = [[BannerView alloc] init];
+    [view5.imgView setImageWithURL:[NSURL URLWithString:@"http://oowantxlb.bkt.clouddn.com/upload/rear/749b4f2a7cc56b5c0068545718fd24d4.png"] placeholderImage:[UIImage imageNamed:@"placeholderImage"]];
+    view5.labelTitle.text = @"讲形势指方向——图解读习近平这次对省部级干部说了啥";
+    
+    PGBanner *banner = [[PGBanner alloc] initViewWithFrame:CGRectMake(0, 0, SCREENWIDTH, 200) ViewList:@[view0, view1, view2, view3, view4, view5] timeInterval:3.0];
+    [self.scrollView addSubview:banner];
+}
+
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.view.backgroundColor = BGCOLOR;
     [self initTabbarWireView];
     [self initNavigationBar];
+    [self initScrollView];
+    [self initBanner];
 }
 
 /*
