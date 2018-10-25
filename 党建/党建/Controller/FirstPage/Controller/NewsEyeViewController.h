845cb9e3e49d2bf0d444ab2de435e52173cbae41
xiaopohaiwx <1309232650@qq.com1309232650@qq.com1309232650@qq.com1309232650@qq.com1309232650@qq.com1309232650@qq.com>
2018-10-25 18:03:56 +0800

    YTQ版本回滚

--- 党建/党建/Controller/FirstPage/Controller/NewsEyeViewController.h
+++ 党建/党建/Controller/FirstPage/Controller/NewsEyeViewController.h
@@ -1,5 +1,5 @@
 //
-//  InformProjectNewsEye.h
+//  NewsEyeViewController.h
 //  党建
 //
 //  Created by 邱敏 on 2018/10/25.
@@ -10,7 +10,7 @@
 
 NS_ASSUME_NONNULL_BEGIN
 
-@interface InformProjectNewsEye : UIViewController<UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate>
+@interface NewsEyeViewController : UIViewController
 
 @end
 
